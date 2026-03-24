47fcbe769286257efd1668ecb5b4942e11ff0733
Rafael Vieira <62526496+rafaaahype@users.noreply.github.com>
2026-03-23 21:32:03 -0300

    questoes pares da semana 1

--- Semana 1/programa6.c
+++ Semana 1/programa6.c
@@ -5,6 +5,6 @@ int main(){
 	printf("Digite o valor do seno de um angulo: ");
 	double seno; scanf("%lf",&seno);
 	double cosseno = sqrt(1-pow(seno,2));
-	printf("O valor absoluto do cosseno eh %.8lf",cosseno);
+	printf("O valor absoluto do cosseno eh %.2lf",cosseno);
 	return 0;
 }
